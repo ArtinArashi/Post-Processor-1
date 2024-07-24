17a7e3f3c1ba8f5ef3764abd7d1d737c7f990de4
ArtinArashi <84471205+ArtinArashi@users.noreply.github.com>
2024-07-24 14:17:16 +0330

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.button3 = new System.Windows.Forms.Button();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.button4 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "\"G Code file (*.cnc)|*.cnc|All files (*.*)|*.*\"";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(382, 308);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "----->";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(29, 132);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(334, 382);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(477, 132);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(336, 382);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(848, 414);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(162, 47);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "&Save Output File ";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(29, 90);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(286, 26);
+            this.textBox3.TabIndex = 5;
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Appearance.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.labelControl1.Appearance.Options.UseFont = true;
+            this.labelControl1.Location = new System.Drawing.Point(29, 51);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(78, 19);
+            this.labelControl1.TabIndex = 6;
+            this.labelControl1.Text = "Input File :";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(321, 90);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(45, 27);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "///";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Appearance.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.labelControl2.Appearance.Options.UseFont = true;
+            this.labelControl2.Location = new System.Drawing.Point(477, 51);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(84, 19);
+            this.labelControl2.TabIndex = 8;
+            this.labelControl2.Text = "Outpit File :";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(848, 479);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(162, 35);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "&Exit";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.CheckFileExists = true;
+            this.saveFileDialog1.Filter = "\"G Code file (*.cnc)|*.cnc|All files (*.*)|*.*\"";
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(768, 91);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(45, 26);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "///";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(476, 91);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(286, 26);
+            this.textBox4.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1022, 544);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.labelControl2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.labelControl1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private System.Windows.Forms.Button button3;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string fileContent , filePath;
+        private IEnumerable<string> inp;
+        private List<string> outp = new List<string>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool go = false;
+                foreach (string a in inp)
+                {
+                    if (a.Contains("G0"))
+                    {
+                        if (go)
+                        {
+                            outp.Add(a);
+                        }
+                        else
+                        {
+                            go = true;
+                            outp.Add("M5");
+                            outp.Add(a);
+                        }
+                    }
+                    else
+                    {
+                        if (go)
+                        {
+                            go = false;
+                            outp.Add("M3");
+                            outp.Add(a);
+                        }
+                        else
+                        {
+                            outp.Add(a);
+                        }
+                    }
+                }
+
+
+            textBox2.Text = String.Join(Environment.NewLine, outp);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(textBox4.Text != "")
+            {
+                string path = textBox4.Text;
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    foreach (string c in outp)
+                    {
+                        sw.WriteLine(c);
+                    }
+                }
+
+                MessageBox.Show("The file is Generated Successfully");
+            }
+            else
+            {
+                MessageBox.Show("Please Enter the file Name !");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string Savefilename;
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                Savefilename = saveFileDialog1.FileName;
+                textBox4.Text = Savefilename;
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //Get the path of specified file
+                filePath = openFileDialog1.FileName;
+                textBox3.Text = filePath;
+
+                //Read the contents of the file into a stream
+                var fileStream = openFileDialog1.OpenFile();
+
+                using (StreamReader reader = new StreamReader(fileStream))
+                {
+                    fileContent = reader.ReadToEnd();
+                }
+
+                inp = File.ReadLines(filePath);
+                textBox1.Text = fileContent;
+            }
+        }
+    }
+}
